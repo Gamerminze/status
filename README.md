9582574d1ee972ba7adde38ff87187329a6280d0
Jan Le <janni468@web.de>
2021-07-07 21:27:58 +0200

    Adding component & minor fixes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Status
+# Status (v1)
 
 This is the repository for the server-status page of Gamerminze.  
 For more information look up the documentation.
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,12 @@
 <template>
-  <v-app>
-    <v-row class="mt-5">
-      <v-col align="center">
-        <v-card height="40%" width="20%">
-          <v-card-title class="justify-center">
-            <h2>Status</h2>
-          </v-card-title>
-          <v-card-text v-if="monitors.length == 0">
-            <v-skeleton-loader v-bind="attrs" type="card"></v-skeleton-loader>
-          </v-card-text>
-          <v-card-text>
-            <v-list-item two-line v-for="monitor in monitors" :key="monitor.id">
-              <v-list-item-content>
-                <v-list-item-title>{{
-                  monitor.friendly_name
-                }}</v-list-item-title>
-                <v-list-item-subtitle v-if="monitor.status == 2"
-                  >Online</v-list-item-subtitle
-                >
-                <v-list-item-subtitle v-else>Online</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-app>
+  <Api />
 </template>
 
 <script>
-import axios from "axios";
+import Api from "../components/Api.vue";
+
 export default {
   name: "Home",
-  data() {
-    return {
-      monitors: [],
-    };
-  },
-  methods: {
-    request() {
-      const config = {
-        method: "get",
-        url: "https://jannes-backend.herokuapp.com/uptimeRobot",
-      };
-      axios(config)
-        .then((response) => {
-          this.handling(response.data.monitors);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    handling(monitors) {
-      for (let monitor of monitors) this.monitors.push(monitor);
-    },
-  },
-  mounted() {
-    this.request();
-  },
+  components: { Api },
 };
 </script>
