471227c779e68df17420dd48f0f669310ca9ecbc
95111 <github@jannes.dev>
2021-07-20 20:26:37 +0200

    Chips added, Info component added

---
+++ src/components/Info.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row class="mt-5 mb-5">
+    <v-col align="center">
+      <v-card height="100%" width="20%">
+        <v-card-text v-if="monitors === null">
+          <v-skeleton-loader type="article"></v-skeleton-loader>
+        </v-card-text>
+        <v-card-text v-else>
+          <h2>There are {{ activeMonitors.length }} out of {{ monitors.length }} monitors active</h2>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Info",
+  props: ["monitors"],
+  data() {
+    return {
+      activeMonitors: [],
+    };
+  },
+    watch: {
+    monitors: function () {
+      for (var i = 0; i < this.monitors.length; i++) {
+        if (this.monitors[i].status === 2) {
+          this.activeMonitors.push(this.monitors[i])
+        }
+      }
+    }
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,31 @@
 <template>
-  <v-row class="mt-5">
+  <v-row>
     <v-col align="center">
-      <v-card height="100%" width="20%">
-        <v-card-title class="justify-center">
-          <h2>Status</h2>
-        </v-card-title>
+      <Info :monitors="monitors" />
+      <v-card height="70%" width="20%">
         <v-card-text v-if="monitors === null">
           <v-skeleton-loader type="article"></v-skeleton-loader>
         </v-card-text>
-        <v-card-text>
+        <v-card-text v-else>
           <v-list-item two-line v-for="monitor in monitors" :key="monitor.id">
             <v-list-item-content>
               <v-list-item-title>{{ monitor.friendly_name }}</v-list-item-title>
               <v-list-item-subtitle v-if="monitor.status == 2"
-                >Online</v-list-item-subtitle
+                ><v-chip class="ma-2" color="success" outlined>
+                  <v-icon left>
+                    mdi-cloud-check
+                  </v-icon>
+                  Online
+                </v-chip></v-list-item-subtitle
               >
-              <v-list-item-subtitle v-else>Offline</v-list-item-subtitle>
+              <v-list-item-subtitle v-else
+                ><v-chip class="ma-2" color="error" outlined>
+                  <v-icon left>
+                    mdi-cloud-question
+                  </v-icon>
+                  Offline
+                </v-chip>
+              </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-card-text>
@@ -25,9 +35,12 @@
 </template>
 
 <script>
+import Info from "../components/Info.vue";
 import { request } from "../scripts/api.js";
+
 export default {
   name: "Home",
+  components: { Info },
   data() {
     return {
       monitors: null,
